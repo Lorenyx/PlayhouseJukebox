d9cc1e21f6fa3a53c03cae78569e3ebed650e2c9
Lore <l0r3nyx@gmail.com>
2021-02-18 21:40:27 -0600

    Format channels.py

--- pytube/models/channels.py
+++ pytube/models/channels.py
@@ -3,16 +3,7 @@ from typing import Optional, Dict
 
 from .items import Localized, Snippet, Status, Content
 
-@dataclass
-class Channels():
-    snippet: Optional[ChannelSnippet] = field(default=None, repr=False)
-    content: Optional[ChannelContent] = field(default=None, repr=False)
-    statistics: Optional[ChannelStatistics] = field(default=None, repr=False)
-    topic: Optional[ChannelTopic] = field(default=None, repr=False)
-    status: Optional[Status] = field(default=None, repr=False)
-    branding: Optional[ChannelBranding] = field(default=None, repr=False)
-    localized: Optional[Localized] = field(default=None, repr=False)
-    
+ 
 @dataclass
 class ChannelsSnippet(Snippet):
     customUrl: Optional[str] = field(default=None, repr=False)
@@ -34,3 +25,14 @@ class ChannelTopic():
 @dataclass
 class ChannelBranding():
     ...
+
+
+@dataclass
+class Channels():
+    snippet: Optional[ChannelSnippet] = field(default=None, repr=False)
+    content: Optional[ChannelContent] = field(default=None, repr=False)
+    statistics: Optional[ChannelStatistics] = field(default=None, repr=False)
+    topic: Optional[ChannelTopic] = field(default=None, repr=False)
+    status: Optional[Status] = field(default=None, repr=False)
+    branding: Optional[ChannelBranding] = field(default=None, repr=False)
+    localized: Optional[Localized] = field(default=None, repr=False)
