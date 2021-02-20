f93accb4ca0d97cbcbb438a8732b84a6635ea3fa
Lore <l0r3nyx@gmail.com>
2021-02-20 10:48:39 -0600

    Update items.py

--- pytube/models/items.py
+++ pytube/models/items.py
@@ -1,14 +1,19 @@
-from dataclasses import InitVar, dataclass, field
-from typing import List, Optional, Dict
-
 import dataclasses as ds
+
+from dataclasses import dataclass
 
 
 @dataclass
 class Base():
     ... 
+
+    @classmethod
+    def field_names(cls) -> List:
+        "Returns a list of the field names"
+        return [f.name for f in ds.fields(cls)]
+
     @classmethod
     def from_response(cls, data: dict):
         "Returns a cls object made from the data provided"
-        data = {k:v for k,v in data.items() if k in cls.__slots__}
+        data = {k:v for k,v in data.items() if k in cls.field_names()}
         return cls(**data)
